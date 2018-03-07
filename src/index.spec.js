f9029e3f7e3b923fd32bf2235c22b067b0bbdb52
Anton Zdanov <azdanov@protonmail.com>
2018-03-07 13:58:00 +0200

    feat: add testing + travis ci

---
+++ src/index.spec.js
@@ -0,0 +1,28 @@
+import { readFile } from 'fs';
+import { promisify } from 'util';
+import cheerio from 'cheerio';
+
+const readFileAsync = promisify(readFile);
+
+describe('index', () => {
+  it('should exist', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe('index.html', () => {
+  let index;
+  let $;
+  beforeAll(async () => {
+    index = await readFileAsync('./src/index.html', 'utf-8');
+    $ = cheerio.load(index);
+  });
+
+  it('should contain h1', () => {
+    expect($('body').find('h1')).toHaveLength(1);
+  });
+
+  it('should say hello', () => {
+    expect($('h1#title').text()).toBe('Hello!');
+  });
+});
